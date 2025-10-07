2e1a701e57f157a13978deff3cadc832d86fad12
Faizaan Qureshi <qfaizaan@gmail.com>
2025-10-07 04:39:04 -0400

    small bug

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const withPWAConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: false, // Enable PWA in development for testing
   buildExcludes: [/middleware-manifest\.json$/],
   runtimeCaching: [
     {
